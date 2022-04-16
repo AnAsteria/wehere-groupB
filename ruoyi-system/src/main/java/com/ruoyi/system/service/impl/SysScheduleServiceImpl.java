f7fdb7268a63645b8f39c6eff461ae2c597f3a74
zhyi930 <374377197@qq.com>
2022-04-16 18:43:44 +0800

    改善了排班表

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysScheduleServiceImpl.java
@@ -0,0 +1,96 @@
+package com.ruoyi.system.service.impl;
+
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.system.domain.SysSchedule;
+import com.ruoyi.system.mapper.SysScheduleMapper;
+import com.ruoyi.system.service.ISysScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 排班表管理Service业务层处理
+ *
+ * @author Group9
+ * @date 2022-03-21
+ */
+@Service
+public class SysScheduleServiceImpl implements ISysScheduleService
+{
+    @Autowired
+    private SysScheduleMapper sysScheduleMapper;
+
+    /**
+     * 查询排班表管理
+     *
+     * @param id 排班表管理主键
+     * @return 排班表管理
+     */
+    @Override
+    public SysSchedule selectSysScheduleById(Long id)
+    {
+        return sysScheduleMapper.selectSysScheduleById(id);
+    }
+
+    /**
+     * 查询排班表管理列表
+     *
+     * @param sysSchedule 排班表管理
+     * @return 排班表管理
+     */
+    @Override
+    public List<SysSchedule> selectSysScheduleList(SysSchedule sysSchedule)
+    {
+        return sysScheduleMapper.selectSysScheduleList(sysSchedule);
+    }
+
+    /**
+     * 新增排班表管理
+     *
+     * @param sysSchedule 排班表管理
+     * @return 结果
+     */
+    @Override
+    public int insertSysSchedule(SysSchedule sysSchedule)
+    {
+        sysSchedule.setCreateTime(DateUtils.getNowDate());
+        return sysScheduleMapper.insertSysSchedule(sysSchedule);
+    }
+
+    /**
+     * 修改排班表管理
+     *
+     * @param sysSchedule 排班表管理
+     * @return 结果
+     */
+    @Override
+    public int updateSysSchedule(SysSchedule sysSchedule)
+    {
+        return sysScheduleMapper.updateSysSchedule(sysSchedule);
+    }
+
+    /**
+     * 批量删除排班表管理
+     *
+     * @param ids 需要删除的排班表管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteSysScheduleByIds(Long[] ids)
+    {
+        return sysScheduleMapper.deleteSysScheduleByIds(ids);
+    }
+
+    /**
+     * 删除排班表管理信息
+     *
+     * @param id 排班表管理主键
+     * @return 结果
+     */
+    @Override
+    public int deleteSysScheduleById(Long id)
+    {
+        return sysScheduleMapper.deleteSysScheduleById(id);
+    }
+}
---
+++ ruoyi-ui/src/views/system/scheduleMS/index.vue
@@ -0,0 +1,271 @@
+<template>
+  <div class="calendar_box">
+    <el-row :gutter="20">
+      <el-col :span="18">
+        <full-calendar ref="fullCalendar" style="height: 100%" :options="calendarOptions"></full-calendar>
+      </el-col>
+    </el-row>
+
+    <!-- 添加或修改 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="值班人" >
+          <el-select v-model="form.userName" placeholder="请选择出诊医生">
+            <el-option
+              v-for="item in userList"
+              :key="item.id"
+              :label="item.nickName"
+              :value="item.userName">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="排班时间" >
+          <el-date-picker
+            v-model="form.time"
+            format="yyyy-MM-dd"
+            value-format="yyyy-MM-dd"
+            type="date"
+            placeholder="选择排班日期">
+          </el-date-picker>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button  @click="dialogFormVisible = false" size="small">取 消</el-button>
+        <el-button type="primary" @click="save" size="small">保 存</el-button>
+      </div>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import {
+  listSchedule,
+  getSchedule,
+  delSchedule,
+  addSchedule,
+  updateSchedule,
+  suListSchedule
+} from "@/api/system/schedule";
+import axios from 'axios'
+import {listUser} from "@/api/system/user";
+export default {
+  name:'FindPaiban',
+  components:{FullCalendar},
+  data() {
+    return {
+      isShowBtn: false,
+      dialogFormVisible:false,
+      form:{
+        id:"",
+        userName:"",
+        time:'',
+      },
+      formLabelWidth:120,
+      calendarOptions: {
+        //   timeGridPlugin  可显示每日时间段
+        height: 700,
+        plugins: [dayGridPlugin, interactionPlugin],
+        headerToolbar: {
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,dayGridWeek,dayGrid",
+          // right: 'agendaWeek,dayGridWeek,dayGrid'
+        },
+        buttonText: {
+          // 设置按钮
+          today: "今天",
+          month: "月",
+          week: "周",
+          dayGrid: "天",
+        },
+        // allDaySlot: false,
+        editable: true,
+        selectable: true,
+        navLinks: true,
+        displayEventEnd: true,       //所有视图显示结束时间
+        initialView: "dayGridMonth", // 设置默认显示月，可选周、日
+        dateClick: this.handleDateClick,
+        eventClick: this.handleEventClick,
+        eventsSet: this.handleEvents,
+        select: this.handleDateSelect,
+        events: [],
+        eventColor: "#f08f00", // 修改日程背景色
+        locale: "zh-cn", // 设置语言
+        weekNumberCalculation: "ISO", // 周数
+        customButtons: {},
+        queryParams: {
+          pageNum: 1,
+          pageSize: 10,
+          userId: null,
+          year: null,
+          month: null,
+          day: null
+        }
+      },
+      scheduleList:[],
+      userList:[],
+      dateRange: [],
+    };
+  },
+
+  mounted(){
+    this.getAllUsers();
+    this.getSchedule();
+  },
+
+  methods: {
+    //获取所有医生
+    getAllUsers(){
+      listUser(this.queryParams).then(response => {
+          this.userList = response.rows;
+        }
+      );
+    },
+
+    //获取排班
+    getSchedule(){
+      suListSchedule(this.queryParams).then(response => {
+        this.scheduleList = response.rows;
+        this.scheduleList.forEach(d => {
+          this.calendarOptions.events.push({
+            id : d.id,
+            start : d.time,
+            end : d.time,
+            color : '#2196F3',
+            background : '#2196F3',
+            title : d.userName
+          })
+        })
+      });
+    },
+    //保存
+    save(){
+      this.$message.info(this.form.userId)
+      addSchedule(this.form).then(response => {
+        this.$modal.msgSuccess("新增成功");
+        this.getSchedule();
+        this.dialogFormVisible = false;
+        this.form = {
+          userId: "",
+          time: ""
+        }
+      });
+    },
+    //点击排班事件
+    handleEventClick(calEvent){
+      this.isShowBtn = true;
+      var data = calEvent.event;
+      console.log(calEvent)
+      this.form.id = data._def.extendedProps.pId,
+        this.form.doctorId = data._def.extendedProps.doctorId,
+        this.form.title = data.title,
+        this.form.time = data._def.extendedProps.pbDate.substring(0,10),
+        this.form.doctorName = data._def.extendedProps.doctorName,
+        this.form.ksName = data._def.extendedProps.ksName,
+        this.form.phone = data._def.extendedProps.phone
+      //this.dialogFormVisible = true;
+      console.log(this.dateToString(calEvent.event.start))
+    },
+    //添加
+    addPaiban(){
+      this.dialogFormVisible = true;
+      this.form = {
+        title:"",
+        ctime:"",
+        doctorId:"",
+        id:'',
+        doctorName:"",
+        phone:"",
+        ksName:''
+      }
+      this.isShowBtn = false;
+    },
+    //修改
+    editPaiban(){
+      this.dialogFormVisible = true;
+    },
+    //删除
+    delData(){
+      console.log(this.form)
+      this.$confirm('确定要删除吗?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        var params = new URLSearchParams();
+        params.append("pId",this.form.id);
+        this.ajax.request({
+          url:'/paiban/del.action',
+          data:params,
+          method:'post'
+        }).then((res)=>{
+          if(!res.data.errorcode){
+            console.log(res.data);
+            this.$message.success("删除成功");
+            this.getSchedule();
+            this.dialogFormVisible = false;
+          }
+        })
+        this.dialogFormVisible = false;
+      });
+    },
+    dateToString(now){
+      var year = now.getFullYear();
+      var month =(now.getMonth() + 1).toString();
+      var day = (now.getDate()).toString();
+      if (month.length == 1) {
+        month = "0" + month;
+      }
+      if (day.length == 1) {
+        day = "0" + day;
+      }
+      var dateTime = year + "-" + month + "-" + day;
+      return dateTime;
+    },
+    denglu(li){
+      let fd = new FormData (document.getElementById("loginform")) ;
+      this.ajax.request({
+        url:'/yonghu/login.action',
+        data:fd,
+        method:'post'
+      }).then((res)=>{
+        if(!res.data.errorcode){
+
+        }
+      }).catch(() => {
+
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.addbtn{
+  margin-top:15px;
+  text-align: left;
+  margin-bottom:15px;
+}
+.calendar_box>>>.el-dialog{
+  width: 450px;
+}
+.calendar_box>>>.el-date-editor.el-input{
+  width: 330px;
+}
+.el-select{
+  width: 100%;
+}
+.detail_box>>>.el-form{
+  border:1px solid #ddd;
+}
+.detail_box>>>.el-form-item__content{
+  text-align: left;
+  color: #333;
+}
+</style>
+
