d23dca3dfb3834cceb238b4b1530e5e8393c3c83
吉俊杰 <jisir1315225948@foxmail.com>
2022-04-17 11:53:36 +0800

    更新访客接口
    访客信息接口更改

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/VisitorController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/VisitorController.java
@@ -1,15 +1,18 @@
 package com.ruoyi.web.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.common.core.page.TableDataInfo;
 import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.client.TIMClient;
 import com.ruoyi.common.utils.poi.ExcelUtil;
 import com.ruoyi.system.domain.Visitor;
 import com.ruoyi.system.service.IVisitorService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,7 +36,7 @@ public class VisitorController extends BaseController
      * 查询访客用户信息列表
      */
     @ApiOperation("查询访客用户信息列表")
-    @PreAuthorize("@ss.hasPermi('system:visitor:list')")
+//    @PreAuthorize("@ss.hasPermi('system:visitor:list')")
     @GetMapping("/list")
     public TableDataInfo list(Visitor visitor)
     {
@@ -71,11 +74,15 @@ public class VisitorController extends BaseController
      * 新增访客用户信息
      */
     @ApiOperation("新增访客用户信息")
-    @PreAuthorize("@ss.hasPermi('system:visitor:add')")
-    @Log(title = "访客用户信息", businessType = BusinessType.INSERT)
+//    @PreAuthorize("@ss.hasPermi('system:visitor:add')")
+//    @Log(title = "访客用户信息", businessType = BusinessType.INSERT)
     @PostMapping
     public AjaxResult add(@RequestBody Visitor visitor)
     {
+        JSONObject params = new JSONObject();
+        params.put("UserID",visitor.getVisitorId());
+        ResponseEntity<JSONObject> result = TIMClient.sendRequest("im_open_login_svc", "account_import", null, params, JSONObject.class);
+//        System.out.println("result = " + result);
         return toAjax(visitorService.insertVisitor(visitor));
     }
 
@@ -83,8 +90,8 @@ public class VisitorController extends BaseController
      * 修改访客用户信息
      */
     @ApiOperation("修改访客用户信息")
-    @PreAuthorize("@ss.hasPermi('system:visitor:edit')")
-    @Log(title = "访客用户信息", businessType = BusinessType.UPDATE)
+//    @PreAuthorize("@ss.hasPermi('system:visitor:edit')")
+//    @Log(title = "访客用户信息", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody Visitor visitor)
     {
